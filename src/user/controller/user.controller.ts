08c026bf80a09aa8314b6a24376c2b149d1e08ad
Zheng Ye <zhengye0118@gmail.com>
2023-04-02 21:36:07 -0500

    added login endpoint and jwt and passwordHash methods

--- src/user/controller/user.controller.ts
+++ src/user/controller/user.controller.ts
@@ -7,7 +7,8 @@ import {
   Delete,
   Put,
 } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { UserService } from '../service/user.service';
 import { User } from '../model/user.interface';
 
@@ -16,8 +17,20 @@ export class UserController {
   constructor(private userService: UserService) {}
 
   @Post()
-  create(@Body() user: User): Observable<User> {
-    return this.userService.create(user);
+  create(@Body() user: User): Observable<User | Object> {
+    return this.userService.create(user).pipe(
+      map((user: User) => user),
+      catchError((err) => of({ error: err.message })),
+    );
+  }
+
+  @Post('login')
+  login(@Body() user: User): Observable<Object> {
+    return this.userService.login(user).pipe(
+      map((jwt: string) => {
+        return { access_token: jwt };
+      }),
+    );
   }
 
   @Get(':id')
